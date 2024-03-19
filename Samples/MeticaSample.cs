c33cbf83124341fe4ac7fabdb4d5b1b49eda1ebc
Giorgos Giannoudovardis <giorgos@metica.com>
2024-03-19 11:49:19 +0000

    feat(sample): Completes and simplifies the code sample

--- Samples/MeticaSample.cs
+++ Samples/MeticaSample.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -22,73 +23,58 @@ namespace Metica.Unity
             canvas.AddComponent<GraphicRaycaster>();
 
             // Create the Button
-            var buttonObj = new GameObject("FetchOffersButton");
-            buttonObj.transform.SetParent(canvas.transform, false);
-            
-            fetchOffersButton = buttonObj.AddComponent<Button>();
-            // fetchOffersButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -25);
-            fetchOffersButton.GetComponent<RectTransform>().sizeDelta = new Vector2(180, 30);
-            fetchOffersButton.transform.SetParent(canvas.transform, false);
-            
-            var buttonText = new GameObject("ButtonText").AddComponent<Text>();
-            buttonText.transform.SetParent(buttonObj.transform, false);
-            
-            buttonText.text = "Fetch Offers";
-            buttonText.alignment = TextAnchor.MiddleCenter;
-            buttonText.color = Color.black;
-            
-            var buttonImage = buttonObj.AddComponent<Image>();
-            buttonImage.color = Color.gray;
-            fetchOffersButton.targetGraphic = buttonImage;
+            var elements = new GameObject("Elements");
+            elements.transform.SetParent(canvas.transform, false);
 
-            // Set the button's text font
-            Font ArialFont = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
-            buttonText.font = ArialFont;
-            buttonText.material = ArialFont.material;
-
-            // Create the Button GameObject
-            // button = new GameObject("Button", typeof(Button)).GetComponent<Button>();
-            // button.transform.SetParent(canvas.transform);
-
-            // Set button size 
-            // RectTransform transform = button.GetComponent<RectTransform>();
-            // transform.sizeDelta = new Vector2(160, 30);
-
-            // Set Button Text
-            // button.gameObject.AddComponent<Text>();
-            // Text buttonText = button.GetComponent<Text>();
-            // buttonText.text = "Click Me!";
-            // buttonText.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-            // buttonText.color = Color.black;
-
-            // Button click action
-            // button.onClick.AddListener(delegate { buttonText.text = "Clicked!"; });
-
-            // Create the Text GameObject
             // textElement = new GameObject("Text", typeof(Text)).GetComponent<Text>();
-            // textElement.transform.SetParent(canvas.transform);
-            //
-            // // Set Text properties
-            // textElement.text = "Not Clicked";
-            // textElement.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-            // textElement.color = Color.white;
-            //
-            // // Set Text Background
-            // textElement.gameObject.AddComponent<Image>();
-            // Image textBackground = textElement.GetComponent<Image>();
-            // textBackground.color = Color.blue;
-            //
-            // // Position the button and the text on the canvas
+            textElement = elements.AddComponent<Text>();
+            textElement.transform.SetParent(canvas.transform, false);
+            textElement.color = Color.white;
+
+            Font legacyFont = (Font)Resources.GetBuiltinResource(typeof(Font), "LegacyRuntime.ttf");
+            textElement.font = legacyFont;
+            textElement.material = legacyFont.material;
+
+            // Position the button and the text on the canvas
             // transform.localPosition = new Vector3(0, 0, 0);
-            // textElement.GetComponent<RectTransform>().localPosition = new Vector3(0, 50, 0);
+            textElement.GetComponent<RectTransform>().localPosition = new Vector3(0, 50, 0);
+
+            MeticaAPI.Initialise("userId", "appId", "apiKey",
+                (result => textElement.text = (result.Result ? "Initialised" : "Failed to initialise")));
         }
 
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.F12))
             {
-                isPanelVisible = !isPanelVisible;
-                canvas.SetActive(isPanelVisible);
+                MeticaAPI.GetOffers(new[] { "main" }, (result) =>
+                {
+                    if (result.Error != null)
+                    {
+                        textElement.text = "Error: " + result.Error;
+                    }
+                    else
+                    {
+                        var resultPlacements = result.Result.placements;
+                        textElement.text = "Offers: " + (resultPlacements.ContainsKey("main") ? resultPlacements["main"].Count : 0) + " offers found";
+                    }
+                });
+            }
+
+            if (Input.GetKeyDown(KeyCode.F9))
+            {
+                textElement.text = "Logging offer display";
+                MeticaAPI.LogOfferDisplay("offerId", "main");
+            }
+
+            if (Input.GetKeyDown(KeyCode.F8))
+            {
+                textElement.text = "Logging user attributes";
+                MeticaAPI.LogUserAttributes(new Dictionary<string, object>()
+                {
+                    { "name", "Joe" },
+                    { "level", 5 },
+                });
             }
         }
     }
