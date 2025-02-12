caca6a95e5df9cadd028cdc740f3ec8c21248994
rstecca-metica <riccardo@metica.com>
2025-02-12 12:24:51 +0000

    Update sample script
    and remove duplicate

--- TestProject/Assets/SampleScript.cs
+++ TestProject/Assets/SampleScript.cs
@@ -1,93 +1,102 @@
-using System.Collections;
 using System.Collections.Generic;
 using Metica.Unity;
 using Newtonsoft.Json;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.UI;
 
 public class SampleScript : MonoBehaviour
 {
+    [SerializeField] private string _userId = string.Empty;
+    [SerializeField] private string _appId = string.Empty;
+    [SerializeField] private string _apiKey = string.Empty;
+    [SerializeField] private SdkConfigProvider _sdkConfiguration;
+
     GameObject canvas;
-        Text textElement;
+    Text textElement;
 
-        void Start()
+    void Start()
+    {
+        Assert.IsFalse(string.IsNullOrEmpty(_userId));
+        Assert.IsFalse(string.IsNullOrEmpty(_appId));
+        Assert.IsFalse(string.IsNullOrEmpty(_apiKey));
+
+        // Create new Canvas GameObject
+        canvas = new GameObject("Canvas");
+        Canvas c = canvas.AddComponent<Canvas>();
+        c.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvas.AddComponent<CanvasScaler>();
+        canvas.AddComponent<GraphicRaycaster>();
+
+        // Create the Button
+        var elements = new GameObject("Elements");
+        elements.transform.SetParent(canvas.transform, false);
+
+        // textElement = new GameObject("Text", typeof(Text)).GetComponent<Text>();
+        textElement = elements.AddComponent<Text>();
+        textElement.transform.SetParent(canvas.transform, false);
+        textElement.color = Color.white;
+
+        Font legacyFont = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+        textElement.font = legacyFont;
+        textElement.material = legacyFont.material;
+
+        // Position the button and the text on the canvas
+        // transform.localPosition = new Vector3(0, 0, 0);
+        textElement.GetComponent<RectTransform>().localPosition = new Vector3(0, 50, 0);
+
+        MeticaAPI.Initialise(_userId, _appId, _apiKey, _sdkConfiguration.SdkConfig,
+            (result => textElement.text = (result.Result ? "Initialised" : "Failed to initialise")));
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
         {
-            // Create new Canvas GameObject
-            canvas = new GameObject("Canvas");
-            Canvas c = canvas.AddComponent<Canvas>();
-            c.renderMode = RenderMode.ScreenSpaceOverlay;
-            canvas.AddComponent<CanvasScaler>();
-            canvas.AddComponent<GraphicRaycaster>();
-
-            // Create the Button
-            var elements = new GameObject("Elements");
-            elements.transform.SetParent(canvas.transform, false);
-
-            // textElement = new GameObject("Text", typeof(Text)).GetComponent<Text>();
-            textElement = elements.AddComponent<Text>();
-            textElement.transform.SetParent(canvas.transform, false);
-            textElement.color = Color.white;
-
-            Font legacyFont = (Font)Resources.GetBuiltinResource(typeof(Font), "LegacyRuntime.ttf");
-            textElement.font = legacyFont;
-            textElement.material = legacyFont.material;
-
-            // Position the button and the text on the canvas
-            // transform.localPosition = new Vector3(0, 0, 0);
-            textElement.GetComponent<RectTransform>().localPosition = new Vector3(0, 50, 0);
-
-            MeticaAPI.Initialise("userId", "appId", "apiKey",
-                (result => textElement.text = (result.Result ? "Initialised" : "Failed to initialise")));
+            MeticaAPI.GetOffers(new[] { "main" }, (result) =>
+            {
+                if (result.Error != null)
+                {
+                    textElement.text = "Error: " + result.Error;
+                }
+                else
+                {
+                    var resultPlacements = result.Result.placements;
+                    textElement.text = "Offers: " + (resultPlacements.ContainsKey("main") ? resultPlacements["main"].Count : 0) + " offers found";
+                }
+            });
         }
 
-        void Update()
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            if (Input.GetKeyDown(KeyCode.F12))
+            MeticaAPI.GetConfig(result =>
             {
-                MeticaAPI.GetOffers(new[] { "main" }, (result) =>
+                if (result.Error != null)
                 {
-                    if (result.Error != null)
-                    {
-                        textElement.text = "Error: " + result.Error;
-                    }
-                    else
-                    {
-                        var resultPlacements = result.Result.placements;
-                        textElement.text = "Offers: " + (resultPlacements.ContainsKey("main") ? resultPlacements["main"].Count : 0) + " offers found";
-                    }
-                });
-            }
-
-            if (Input.GetKeyDown(KeyCode.F10))
-            {
-                MeticaAPI.GetConfig(result =>
+                    textElement.text = "GetConfig Error: " + result.Error;
+                }
+                else
                 {
-                    if (result.Error != null)
-                    {
-                        textElement.text = "GetConfig Error: " + result.Error;
-                    }
-                    else
-                    {
-                        var configStr = JsonConvert.SerializeObject(result.Result);
-                        textElement.text = "Config: " + configStr;
-                    }
-                });
-            }
-
-            if (Input.GetKeyDown(KeyCode.F9))
-            {
-                textElement.text = "Logging offer display";
-                MeticaAPI.LogOfferDisplay("offerId", "main");
-            }
-
-            if (Input.GetKeyDown(KeyCode.F8))
-            {
-                textElement.text = "Logging user attributes";
-                MeticaAPI.LogUserAttributes(new Dictionary<string, object>()
-                {
-                    { "name", "Joe" },
-                    { "level", 5 },
-                });
-            }
+                    var configStr = JsonConvert.SerializeObject(result.Result);
+                    textElement.text = "Config: " + configStr;
+                }
+            });
         }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            textElement.text = "Logging offer display";
+            MeticaAPI.LogOfferDisplay("offerId", "main");
+        }
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            textElement.text = "Logging user attributes";
+            MeticaAPI.LogUserAttributes(new Dictionary<string, object>()
+            {
+                { "name", "Joe" },
+                { "level", 5 },
+            });
+        }
+    }
 }
