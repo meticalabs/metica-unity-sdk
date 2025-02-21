9c641e60f82863d57cb805b18cb54dac793c5403
rstecca-metica <riccardo@metica.com>
2025-02-21 10:55:50 +0000

    Suppress TestDisplayLogApplication test
    It needs to be rebuilt.

--- SDK/Tests/Runtime/OffersManagerTest.cs
+++ SDK/Tests/Runtime/OffersManagerTest.cs
@@ -113,95 +113,95 @@ namespace MeticaUnitySDK.SDK.Tests.Runtime
             });
         }
 
-        [Test]
-        public void TestDisplayLogApplication()
-        {
-            TestOps testOps = new TestOps
-            {
-                FailOp = false,
-                ShouldBeInvoked = true
-            };
-            testOps.MockOffers = new OffersByPlacement()
-            {
-                placements = new Dictionary<string, List<Offer>>()
-                {
-                    { "p1", new List<Offer> {
-                        new()
-                        {
-                            offerId = "offerP1",
-                            displayLimits = new List<DisplayLimit>
-                            {
-                                new()
-                                {
-                                    timeWindowInHours = 24, maxDisplayCount = 2
-                                }
-                            },
-                            metrics = new OfferMetrics()
-                            {
-                                display = new DisplayMetric()
-                                {
-                                    userId = MeticaAPI.UserId,
-                                    appId = MeticaAPI.AppId,
-                                    meticaAttributes = new MeticaAttributes()
-                                    {
-                                        offer = new OfferVariant()
-                                        {
-                                            offerId = "offerP1",
-                                            bundleId = "bundleP1",
-                                            variantId = "variantP1",
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }},
-                    { "p2", new List<Offer> {
-                        new()
-                        {
-                            offerId = "offerP2",
-                        }
-                    }}
-                }
-            };
+        // [Test]
+        // public void TestDisplayLogApplication()
+        // {
+        //     TestOps testOps = new TestOps
+        //     {
+        //         FailOp = false,
+        //         ShouldBeInvoked = true
+        //     };
+        //     testOps.MockOffers = new OffersByPlacement()
+        //     {
+        //         placements = new Dictionary<string, List<Offer>>()
+        //         {
+        //             { "p1", new List<Offer> {
+        //                 new()
+        //                 {
+        //                     offerId = "offerP1",
+        //                     displayLimits = new List<DisplayLimit>
+        //                     {
+        //                         new()
+        //                         {
+        //                             timeWindowInHours = 24, maxDisplayCount = 2
+        //                         }
+        //                     },
+        //                     metrics = new OfferMetrics()
+        //                     {
+        //                         display = new DisplayMetric()
+        //                         {
+        //                             userId = MeticaAPI.UserId,
+        //                             appId = MeticaAPI.AppId,
+        //                             meticaAttributes = new MeticaAttributes()
+        //                             {
+        //                                 offer = new OfferVariant()
+        //                                 {
+        //                                     offerId = "offerP1",
+        //                                     bundleId = "bundleP1",
+        //                                     variantId = "variantP1",
+        //                                 }
+        //                             }
+        //                         }
+        //                     }
+        //                 }
+        //             }},
+        //             { "p2", new List<Offer> {
+        //                 new()
+        //                 {
+        //                     offerId = "offerP2",
+        //                 }
+        //             }}
+        //         }
+        //     };
 
-            MeticaAPI.BackendOperations = testOps;
-            var offersManager = new OffersManager();
+        //     MeticaAPI.BackendOperations = testOps;
+        //     var offersManager = new OffersManager();
 
-            // fetches the offers once
-            offersManager.GetOffers(new[] { "p1", "p2" }, result =>
-            {
-                Assert.Null(result.Error);
-                Assert.NotNull(result.Result);
-                Assert.AreEqual("offerP1", result.Result.placements["p1"][0].offerId);
-                Assert.AreEqual("offerP2", result.Result.placements["p2"][0].offerId);
+        //     // fetches the offers once
+        //     offersManager.GetOffers(new[] { "p1", "p2" }, result =>
+        //     {
+        //         Assert.Null(result.Error);
+        //         Assert.NotNull(result.Result);
+        //         Assert.AreEqual("offerP1", result.Result.placements["p1"][0].offerId);
+        //         Assert.AreEqual("offerP2", result.Result.placements["p2"][0].offerId);
                 
-                // Assert.That(MeticaAPI.DisplayLog.GetEntriesForOffer("offerP1").Count == 1);
-                // // because it has no display limits specified
-                // Assert.That(MeticaAPI.DisplayLog.GetEntriesForOffer("offerP2").Count == 0);
-            });
+        //         // Assert.That(MeticaAPI.DisplayLog.GetEntriesForOffer("offerP1").Count == 1);
+        //         // // because it has no display limits specified
+        //         // Assert.That(MeticaAPI.DisplayLog.GetEntriesForOffer("offerP2").Count == 0);
+        //     });
             
-            testOps.ShouldBeInvoked = false;
-            testOps.WasInvoked = false;
+        //     testOps.ShouldBeInvoked = false;
+        //     testOps.WasInvoked = false;
             
-            // fetches again, this time from the cache
-            offersManager.GetOffers(new[] { "p1", "p2" }, result =>
-            {
-                Assert.Null(result.Error);
-                Assert.NotNull(result.Result);
-                Assert.AreEqual("offerP1", result.Result.placements["p1"][0].offerId);
-                Assert.AreEqual("offerP2", result.Result.placements["p2"][0].offerId);
-            });
+        //     // fetches again, this time from the cache
+        //     offersManager.GetOffers(new[] { "p1", "p2" }, result =>
+        //     {
+        //         Assert.Null(result.Error);
+        //         Assert.NotNull(result.Result);
+        //         Assert.AreEqual("offerP1", result.Result.placements["p1"][0].offerId);
+        //         Assert.AreEqual("offerP2", result.Result.placements["p2"][0].offerId);
+        //     });
             
-            // fetches one more time, again from the cache
-            // offerP1 should not be returned at it reached the limit. offerP1 should be returned
-            offersManager.GetOffers(new[] { "p1", "p2" }, result =>
-            {
-                Assert.Null(result.Error);
-                Assert.NotNull(result.Result);
-                Assert.AreEqual(0, result.Result.placements["p1"].Count);
-                Assert.AreEqual("offerP2", result.Result.placements["p2"][0].offerId);
-            });
-        }
+        //     // fetches one more time, again from the cache
+        //     // offerP1 should not be returned at it reached the limit. offerP1 should be returned
+        //     offersManager.GetOffers(new[] { "p1", "p2" }, result =>
+        //     {
+        //         Assert.Null(result.Error);
+        //         Assert.NotNull(result.Result);
+        //         Assert.AreEqual(0, result.Result.placements["p1"].Count);
+        //         Assert.AreEqual("offerP2", result.Result.placements["p2"][0].offerId);
+        //     });
+        // }
 
         [Test]
         public void TestErrorsAreReported()
