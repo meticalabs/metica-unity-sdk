8cfe85d030670d5aa4f6f43937244e85a4751b53
rstecca-metica <riccardo@metica.com>
2025-03-13 14:44:20 +0000

    Un-nested a class
    in OfferManager and ConfigManager

--- SDK/Runtime/Experimental/ConfigManager.cs
+++ SDK/Runtime/Experimental/ConfigManager.cs
@@ -7,37 +7,37 @@ using Newtonsoft.Json;
 
 namespace Metica.Experimental
 {
+    [System.Serializable]
+    public struct ConfigResult : IMeticaSdkResult
+    {
+        // TODO : fields should be readonly or with private setter
+        [JsonExtensionData] // Needed when we want the root (anonymous dictionary in this case) to go in a specific field
+        public Dictionary<string, object> Configs { get; set; }
+
+        [JsonIgnore] public HttpResponse.ResultStatus Status { get; set; }
+        [JsonIgnore] public string Error { get; set; }
+        [JsonIgnore] public string RawContent { get; set; }
+
+        public override string ToString()
+        {
+            string configString = string.Empty;
+            if(Configs != null)
+            {
+                foreach (var c in Configs)
+                {
+                    configString = $"{configString}{c.Key} : {c.Value}\n";
+                }
+            }
+            return $"{nameof(ConfigResult)} [{Configs?.Count}]:\n{configString}\n Status: {Status}\n RawContent: {RawContent}\n Error: {Error}";
+        }
+    }
+
     public sealed class ConfigManager : EndpointManager
     {
         public ConfigManager(IHttpService httpService, string endpoint) : base(httpService, endpoint)
         {
         }
-
-        [System.Serializable]
-        public struct ConfigResult : IMeticaSdkResult
-        {
-            // TODO : fields should be readonly or with private setter
-            [JsonExtensionData] // Needed when we want the root (anonymous dictionary in this case) to go in a specific field
-            public Dictionary<string, object> Configs { get; set; }
-
-            [JsonIgnore] public HttpResponse.ResultStatus Status { get; set; }
-            [JsonIgnore] public string Error { get; set; }
-            [JsonIgnore] public string RawContent { get; set; }
-
-            public override string ToString()
-            {
-                string configString = string.Empty;
-                if(Configs != null)
-                {
-                    foreach (var c in Configs)
-                    {
-                        configString = $"{configString}{c.Key} : {c.Value}\n";
-                    }
-                }
-                return $"{nameof(ConfigResult)} [{Configs?.Count}]:\n{configString}\n Status: {Status}\n RawContent: {RawContent}\n Error: {Error}";
-            }
-        }
-
+        
         public async Task<ConfigResult> GetConfigsAsync(string userId, List<string> configKeys = null, Dictionary<string, object> userProperties = null, DeviceInfo deviceInfo = null)
         {
             var requestBody = new Dictionary<string, object>
