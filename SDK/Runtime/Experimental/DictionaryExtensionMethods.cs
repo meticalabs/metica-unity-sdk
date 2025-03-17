5ef83a26aa40b18cb6bf5282a250e3fe666bf8cb
rstecca-metica <riccardo@metica.com>
2025-03-17 18:42:03 +0000

    Dictionary extension methods

---
+++ SDK/Runtime/Experimental/DictionaryExtensionMethods.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace Metica.Unity
+{
+    public static class DictionaryExtensionMethods
+    {
+        public static void Concat<T, S>(this Dictionary<T,S> dictionary, Dictionary<T,S> additionalDictionary, bool overwriteExistingKeys = false)
+        {
+            if(additionalDictionary == null)
+            {
+                throw new ArgumentNullException();
+            }
+            foreach (var item in additionalDictionary)
+            {
+                if (dictionary.ContainsKey(item.Key))
+                {
+                    if (overwriteExistingKeys)
+                    {
+                        dictionary[item.Key] = item.Value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Duplicate key: {item.Key}");
+                    }
+                }
+                else
+                {
+                    dictionary.Add(item.Key, item.Value); // lets exceptions bubble up
+                }
+            }
+        }
+    }
+}
