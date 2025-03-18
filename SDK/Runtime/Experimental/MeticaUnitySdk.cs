4e18af19f1df6848c777f550923a44ad1e347b8b
rstecca-metica <riccardo@metica.com>
2025-03-18 16:20:52 +0000

    Add some tests to `MeticaUnitySdk`

--- SDK/Runtime/Experimental/MeticaUnitySdk.cs
+++ SDK/Runtime/Experimental/MeticaUnitySdk.cs
@@ -31,11 +31,31 @@ namespace Metica.Experimental.Unity
             _meticaSdk.LogPurchaseEventWithProductId("mega_offer_123", "EUR", 1.99);
             _meticaSdk.LogOfferPurchaseEvent("generic", "23851", "EUR", 9.99);
 
+            _meticaSdk.LogFullStateUserUpdateEvent(
+                new()
+                {
+                    { "level", 91.0 },
+                    { "faction", "Dragoons" },
+                    { "gear_slot_LH", "Sword of Diverging Fates" },
+                    { "gear_slot_RH", "Sword of the Thalos Masters" },
+                    { "gear_slot_NK", "Second Soul Talisman" },
+                    { "gear_slot_FT", "Dragonscale Boots of Zeal" },
+                    { "gear_slot_BD", "Spiked Cape of Kuhaman" },
+                    { "gear_slot_HD", "Shadow Mask of the Crow" },
+                    { "gear_slot_LR", "Diamond Ring of Blood Pacts" },
+                    { "gear_slot_RR", "The Sink Of Purity" },
+                    { "gear_slot_BT", "Dragonscale Belt of Madness"},
+                    { "last_map", "Thalosean Cemetery" },
+                    { "totalDeaths", 920 },
+                    { "totalWalkingDistance", 20038.82}
+                });
+
             _meticaSdk.LogPartialStateUserUpdateEvent(
                 new()
                 {
                     { "level", 100.0 },
                     { "faction", "Dragoons" },
+                    { "totalDeaths", 982 }
                 });
         }
     }
