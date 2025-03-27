81b90b60160cef2914872d9484d54fd0b3debefe
rstecca-metica <riccardo@metica.com>
2025-03-27 10:30:19 +0000

    Editor asmdef fixes

--- SDK/Editor/PreBuild.cs
+++ SDK/Editor/PreBuild.cs
@@ -1,14 +1,17 @@
-//using UnityEditor.Build;
-//using UnityEditor.Build.Reporting;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
 
-//using Metica.Unity;
+using Metica.Unity;
 
-//class PrebuildProcessor : IPreprocessBuildWithReport
-//{
-//    public int callbackOrder { get { return 0; } }
-//    public void OnPreprocessBuild(BuildReport report)
-//    {
-//        UnityEngine.Debug.Log("BUILDING");
-//        MeticaAPI.WriteJsonSdkInfo();
-//    }
-//}
+namespace Metica.UnityEditor
+{
+    class PrebuildProcessor : IPreprocessBuildWithReport
+    {
+        public int callbackOrder { get { return 0; } }
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            UnityEngine.Debug.Log("BUILDING");
+            MeticaAPI.WriteJsonSdkInfo();
+        }
+    }
+}
