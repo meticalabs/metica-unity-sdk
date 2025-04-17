b343b4b07211deb663531ef3def61c32dd182c32
Anton Urankar <124867229+antonurankar-moloco@users.noreply.github.com>
2025-04-17 21:33:42 +0200

    Move MeticaAds to MeticaSDK

---
+++ Runtime/ADS/Android/ShowCallbackProxy.cs
@@ -0,0 +1,63 @@
+// ShowCallbackProxy.cs
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Metica.ADS
+{
+public class ShowCallbackProxy : AndroidJavaProxy
+{    private const string TAG = MeticaAds.TAG;
+    private readonly TaskCompletionSource<bool> _tcs;
+    
+    // Events for all callbacks
+    public event Action<string> AdShowSuccess;
+    public event Action<string, string> AdShowFailed;
+    public event Action<string> AdHidden;
+    public event Action<string> AdClicked;
+    public event Action<string> AdRewarded;
+    
+    public ShowCallbackProxy(TaskCompletionSource<bool> tcs) 
+        : base("com.metica.ads.MeticaShowAdCallback")
+    {
+        _tcs = tcs;
+        Debug.Log($"{TAG} ShowCallbackProxy created");
+    }
+    
+    // Called when ad shows successfully
+    public void onAdShowSuccess(string adUnitId)
+    {
+        Debug.Log($"{TAG} onAdShowSuccess callback received for adUnitId={adUnitId}");
+        AdShowSuccess?.Invoke(adUnitId);
+        _tcs.SetResult(true);
+    }
+    
+    // Called when ad fails to show
+    public void onAdShowFailed(string adUnitId, string error)
+    {
+        Debug.Log($"{TAG} onAdShowFailed callback received for adUnitId={adUnitId}, error={error}");
+        AdShowFailed?.Invoke(adUnitId, error);
+        _tcs.SetResult(false);
+    }
+    
+    // Called when ad is hidden (closed)
+    public void onAdHidden(string adUnitId)
+    {
+        Debug.Log($"{TAG} onAdHidden callback received for adUnitId={adUnitId}");
+        AdHidden?.Invoke(adUnitId);
+    }
+    
+    // Called when ad is clicked
+    public void onAdClicked(string adUnitId)
+    {
+        Debug.Log($"{TAG} onAdClicked callback received for adUnitId={adUnitId}");
+        AdClicked?.Invoke(adUnitId);
+    }
+    
+    // Called when ad provides a reward
+    public void onAdRewarded(string adUnitId)
+    {
+        Debug.Log($"{TAG} onAdRewarded callback received for adUnitId={adUnitId}");
+        AdRewarded?.Invoke(adUnitId);
+    }
+}
+}
---
+++ Runtime/ADS/MeticaAds.cs
@@ -0,0 +1,60 @@
+#nullable enable
+
+using UnityEngine;
+using System;
+using System.Threading.Tasks;
+
+namespace Metica.ADS 
+{
+    public static class MeticaAds
+    {   
+        public const string TAG = "MeticaUnityPlugin";
+        private static readonly PlatformDelegate platformDelegate;
+
+        static MeticaAds()
+        {
+#if UNITY_ANDROID
+            platformDelegate = new AndroidDelegate();
+#endif
+
+#if UNITY_IOS
+            // TODO
+            platformDelegate = new IOSDelegate();
+#endif
+            platformDelegate.InterstitialAdLoadSuccess += MeticaAdsCallbacks.Interstitial.OnAdLoadSuccessInternal;
+            platformDelegate.InterstitialAdLoadFailed += MeticaAdsCallbacks.Interstitial.OnAdLoadFailedInternal;
+            platformDelegate.InterstitialAdShowSuccess += MeticaAdsCallbacks.Interstitial.OnAdShowSuccessInternal;
+            platformDelegate.InterstitialAdShowFailed += MeticaAdsCallbacks.Interstitial.OnAdShowFailedInternal;
+            platformDelegate.InterstitialAdHidden += MeticaAdsCallbacks.Interstitial.OnAdHiddenInternal;
+            platformDelegate.InterstitialAdClicked += MeticaAdsCallbacks.Interstitial.OnAdClickedInternal;
+        }
+
+        public static void SetLogEnabled(bool logEnabled) 
+        {
+            platformDelegate.SetLogEnabled(logEnabled);
+        }
+        
+        public static async Task<bool> LoadInterstitialAsync()
+        {
+            return await platformDelegate.LoadInterstitialAsync();
+        }
+        public static async Task<bool> ShowInterstitialAsync()
+        {
+            return await platformDelegate.ShowInterstitialAsync();
+        }
+    }
+
+    internal interface PlatformDelegate {
+        // Events for interstitial ad lifecycle callbacks
+        public event Action<string> InterstitialAdLoadSuccess;
+        public event Action<string, string> InterstitialAdLoadFailed;
+        public event Action<string> InterstitialAdShowSuccess;
+        public event Action<string, string> InterstitialAdShowFailed;
+        public event Action<string> InterstitialAdHidden;
+        public event Action<string> InterstitialAdClicked;
+        
+        void SetLogEnabled(bool logEnabled);
+        Task<bool> LoadInterstitialAsync();
+        Task<bool> ShowInterstitialAsync();
+    }
+}
