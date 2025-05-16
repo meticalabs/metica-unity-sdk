f7d05b1fba493ad27684c7c8598986836c128b5c
Anton Urankar <124867229+antonurankar-moloco@users.noreply.github.com>
2025-05-16 10:04:39 +0200

    Add Rewarded Ad API (#52)

--- Runtime/ADS/MeticaAds.cs
+++ Runtime/ADS/MeticaAds.cs
@@ -22,12 +22,22 @@ namespace Metica.ADS
             // TODO
             platformDelegate = new IOSDelegate();
 #endif
+            // Interstitial ad callbacks
             platformDelegate.InterstitialAdLoadSuccess += MeticaAdsCallbacks.Interstitial.OnAdLoadSuccessInternal;
             platformDelegate.InterstitialAdLoadFailed += MeticaAdsCallbacks.Interstitial.OnAdLoadFailedInternal;
             platformDelegate.InterstitialAdShowSuccess += MeticaAdsCallbacks.Interstitial.OnAdShowSuccessInternal;
             platformDelegate.InterstitialAdShowFailed += MeticaAdsCallbacks.Interstitial.OnAdShowFailedInternal;
             platformDelegate.InterstitialAdHidden += MeticaAdsCallbacks.Interstitial.OnAdHiddenInternal;
             platformDelegate.InterstitialAdClicked += MeticaAdsCallbacks.Interstitial.OnAdClickedInternal;
+            
+            // Rewarded ad callbacks
+            platformDelegate.RewardedAdLoadSuccess += MeticaAdsCallbacks.Rewarded.OnAdLoadSuccessInternal;
+            platformDelegate.RewardedAdLoadFailed += MeticaAdsCallbacks.Rewarded.OnAdLoadFailedInternal;
+            platformDelegate.RewardedAdShowSuccess += MeticaAdsCallbacks.Rewarded.OnAdShowSuccessInternal;
+            platformDelegate.RewardedAdShowFailed += MeticaAdsCallbacks.Rewarded.OnAdShowFailedInternal;
+            platformDelegate.RewardedAdHidden += MeticaAdsCallbacks.Rewarded.OnAdHiddenInternal;
+            platformDelegate.RewardedAdClicked += MeticaAdsCallbacks.Rewarded.OnAdClickedInternal;
+            platformDelegate.RewardedAdRewarded += MeticaAdsCallbacks.Rewarded.OnAdRewardedInternal;
         }
 
         public static async Task<bool> InitializeAsync()
@@ -39,6 +49,7 @@ namespace Metica.ADS
             platformDelegate.SetLogEnabled(logEnabled);
         }
         
+        // Interstitial ad methods
         public static void LoadInterstitial()
         {
             platformDelegate.LoadInterstitial();
@@ -47,10 +58,23 @@ namespace Metica.ADS
         {
             platformDelegate.ShowInterstitial();
         }
-
         public static bool IsInterstitialReady()
         {
             return platformDelegate.IsInterstitialReady();
+        }
+        
+        // Rewarded ad methods
+        public static void LoadRewarded()
+        {
+            platformDelegate.LoadRewarded();
+        }
+        public static void ShowRewarded()
+        {
+            platformDelegate.ShowRewarded();
+        }
+        public static bool IsRewardedReady()
+        {
+            return platformDelegate.IsRewardedReady();
         }
     }
 
@@ -62,11 +86,27 @@ namespace Metica.ADS
         public event Action<string, string> InterstitialAdShowFailed;
         public event Action<string> InterstitialAdHidden;
         public event Action<string> InterstitialAdClicked;
+        
+        // Events for rewarded ad lifecycle callbacks
+        public event Action<string> RewardedAdLoadSuccess;
+        public event Action<string, string> RewardedAdLoadFailed;
+        public event Action<string> RewardedAdShowSuccess;
+        public event Action<string, string> RewardedAdShowFailed;
+        public event Action<string> RewardedAdHidden;
+        public event Action<string> RewardedAdClicked;
+        public event Action<string> RewardedAdRewarded;
 
         Task<bool> InitializeAsync(string apiKey, string appId, string userId, string version, string baseEndpoint);
         void SetLogEnabled(bool logEnabled);
+        
+        // Interstitial methods
         void LoadInterstitial();
         void ShowInterstitial();
         bool IsInterstitialReady();
+        
+        // Rewarded methods
+        void LoadRewarded();
+        void ShowRewarded();
+        bool IsRewardedReady();
     }
 }
--- Runtime/ADS/MeticaAdsCallbacks.cs
+++ Runtime/ADS/MeticaAdsCallbacks.cs
@@ -48,7 +48,50 @@ public static class MeticaAdsCallbacks
 
     public static class Rewarded
     {
-        // TODO
+        // Public events for consumers to subscribe to
+        public static event Action<string> OnAdLoadSuccess;
+        public static event Action<string, string> OnAdLoadFailed;
+        public static event Action<string> OnAdShowSuccess;
+        public static event Action<string, string> OnAdShowFailed;
+        public static event Action<string> OnAdHidden;
+        public static event Action<string> OnAdClicked;
+        public static event Action<string> OnAdRewarded; // Specific to rewarded ads - user received reward
+
+        // Internal methods to trigger the events - these can only be called from within the assembly
+        internal static void OnAdLoadSuccessInternal(string adUnitId)
+        {
+            OnAdLoadSuccess?.Invoke(adUnitId);
+        }
+
+        internal static void OnAdLoadFailedInternal(string adUnitId, string error)
+        {
+            OnAdLoadFailed?.Invoke(adUnitId, error);
+        }      
+        
+        internal static void OnAdShowSuccessInternal(string adUnitId)
+        {
+            OnAdShowSuccess?.Invoke(adUnitId);
+        }
+
+        internal static void OnAdShowFailedInternal(string adUnitId, string error)
+        {
+            OnAdShowFailed?.Invoke(adUnitId, error);
+        }
+
+        internal static void OnAdHiddenInternal(string adUnitId)
+        {
+            OnAdHidden?.Invoke(adUnitId);
+        }
+
+        internal static void OnAdClickedInternal(string adUnitId)
+        {
+            OnAdClicked?.Invoke(adUnitId);
+        }
+        
+        internal static void OnAdRewardedInternal(string adUnitId)
+        {
+            OnAdRewarded?.Invoke(adUnitId);
+        }
     }
 
     public static class Banner
