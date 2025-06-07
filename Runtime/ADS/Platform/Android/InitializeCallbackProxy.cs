516f3d876807ec569ba35b8ed034c6eabfdb4e4a
Anton Urankar <124867229+antonurankar-moloco@users.noreply.github.com>
2025-06-07 22:58:16 +0200

    ILRD support

--- Runtime/ADS/Platform/Android/InitializeCallbackProxy.cs
+++ Runtime/ADS/Platform/Android/InitializeCallbackProxy.cs
@@ -1,15 +1,16 @@
-// LoadCallbackProxy.cs
+// InitializeCallbackProxy.cs
 
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Metica.ADS
 {
+[SuppressMessage("ReSharper", "InconsistentNaming")]
 public class InitializeCallbackProxy : AndroidJavaProxy
 {
     private const string TAG = MeticaAds.TAG;
     private readonly TaskCompletionSource<bool> _tcs;
-
 
     public InitializeCallbackProxy(TaskCompletionSource<bool> tcs)
         : base("com.metica.ads.MeticaAdsInitCallback")
@@ -18,11 +19,18 @@ public class InitializeCallbackProxy : AndroidJavaProxy
         _tcs = tcs;
     }
 
-    // Called from Android when ad loads successfully
+    // Called from Android when initialization succeeds
     public void onInitialized(bool enabled)
     {
         Debug.Log($"{TAG} onInitialized: {enabled}");
         _tcs.SetResult(enabled);
+    }
+
+    // Called from Android when initialization fails
+    public void onFailed(string reason)
+    {
+        Debug.Log($"{TAG} onFailed: {reason}");
+        _tcs.SetResult(false);
     }
 }
 }
