e6ce503834fb256b8066ed71113cbfc82020d0f9
rstecca-metica <riccardo@metica.com>
2025-10-03 16:23:06 +0100

    MeticaSdk: static wrappers for Events, SmartConfigs, Offers and Ads methods.

--- Runtime/SDK/MeticaSdk.cs
+++ Runtime/SDK/MeticaSdk.cs
@@ -35,25 +35,24 @@ namespace Metica.SDK
     {
         #region Fields
 
-        public static MeticaSdk SDK { get; private set; } = null;
+        private static MeticaSdk Sdk { get; set; } = null;
 
         public static string Version { get => "1.13.4"; }
 
-        public static string UserId {  get; set; } // TODO: set should become private and require a reinitialization to cheange user id
+        public static string UserId { get; set; } // TODO: set should become private and require a reinitialization to cheange user id
         public static string ApiKey { get; private set; }
         public static string AppId { get; private set; }
         // TODO: remove apploving key fromm here in favour of
         // more configurability considering possible additions of other ad providers.
-        public static string ApplovinKey { get; private set; }
         public static string BaseEndpoint { get; private set; }
+        public static string ApplovinKey { get; private set; }
 
         private readonly IHttpService _http;
-        internal readonly OfferManager _offerManager;
-        internal readonly ConfigManager _configManager;
-        internal readonly EventManager _eventManager;
+        private readonly OfferManager _offerManager;
+        private readonly ConfigManager _configManager;
+        private readonly EventManager _eventManager;
 
         public static bool IsMeticaAdsEnabled { get; private set; }
-
 
         #endregion Fields
 
@@ -69,7 +68,6 @@ namespace Metica.SDK
             BaseEndpoint = null;
         }
 
-        
         private static bool CheckConfig(SdkConfig config)
         {
             if (string.IsNullOrEmpty(config.apiKey) || string.IsNullOrEmpty(config.appId) || string.IsNullOrEmpty(config.baseEndpoint))
@@ -92,11 +90,11 @@ namespace Metica.SDK
         {
             RegisterServices(config);
             CheckConfig(config);
-            if (SDK != null)
+            if (Sdk != null)
             {
                 Log.Warning(() => "Metica SDK reinitialized. This means a new initialization was done on top of a previous one.");
             }
-            SDK = new MeticaSdk(config);
+            Sdk = new MeticaSdk(config);
 
             // ADS
             var result = await MeticaAds.InitializeAsync(new MeticaConfiguration()); // TODO: placeholder for configuration (should be SdkConfig)
@@ -113,20 +111,23 @@ namespace Metica.SDK
         // }
 
         /// <summary>
-        /// Registration of implementation of services
+        /// Registration of implementation of services.
         /// </summary>
         /// <param name="config"></param>
+        /// <remarks>Call this before <see cref="InitializeAsync(SdkConfig)"/>
+        /// if and only if you want to use your own implementations of services known
+        /// to <see cref="MeticaSdk"/>, for example to mock them in unit testing.</remarks>
         public static void RegisterServices(SdkConfig config)
         {
-            Registry.Register<IDeviceInfoProvider>(new DeviceInfoProvider());
-            Registry.Register<ILog>(new MeticaLogger(config.logLevel));
+            Registry.RegisterIfNull<IDeviceInfoProvider>(new DeviceInfoProvider());
+            Registry.RegisterIfNull<ILog>(new MeticaLogger(config.logLevel));
         }
 
         /// <summary>
         /// ORIGINAL SDK INITIALIZATION
         /// </summary>
         /// <param name="config">Metica SDK configuration object.</param>
-        public MeticaSdk(SdkConfig config)
+        private MeticaSdk(SdkConfig config)
         {
             // The following part will be soon be obsolete as
             // it'll be implemented in MeticaAds.
@@ -151,157 +152,217 @@ namespace Metica.SDK
             BaseEndpoint = config.baseEndpoint;
         }
 
-        public async Task<OfferResult> GetOffersAsync(string[] placements, Dictionary<string, object> userData = null)
-            => await _offerManager.GetOffersAsync(UserId, placements, userData);
 
-        internal async Task<ConfigResult> GetConfigsAsync(List<string> configKeys = null, Dictionary<string, object> userProperties = null)
-            => await _configManager.GetConfigsAsync(UserId, configKeys, userProperties);
+        public static class Offers
+        {
+            public static async Task<OfferResult> GetOffersAsync(string[] placements, Dictionary<string, object> userData = null)
+                => await Sdk._offerManager.GetOffersAsync(UserId, placements, userData);
+        }
 
-        public void LogLoginEvent(Dictionary<string, object> customPayload = null)
-            => _eventManager.QueueEventAsync(
-                UserId,
-                AppId,
-                EventTypes.Login,
-                null,
-                customPayload);
+        public static class SmartConfig
+        {
+            public static async Task<ConfigResult> GetConfigsAsync(List<string> configKeys = null, Dictionary<string, object> userProperties = null)
+                => await Sdk._configManager.GetConfigsAsync(UserId, configKeys, userProperties);
+        }
 
+        public static class Events
+        {
+            public static void LogLoginEvent(Dictionary<string, object> customPayload = null)
+                => Sdk._eventManager.QueueEventAsync(
+                    UserId,
+                    AppId,
+                    EventTypes.Login,
+                    null,
+                    customPayload);
 
-        public void LogInstallEvent(Dictionary<string, object> customPayload = null)
-            => _eventManager.QueueEventAsync(
-                UserId,
-                AppId,
-                EventTypes.Install,
-                null,
-                customPayload);
+            public static void LogInstallEvent(Dictionary<string, object> customPayload = null)
+                => Sdk._eventManager.QueueEventAsync(
+                    UserId,
+                    AppId,
+                    EventTypes.Install,
+                    null,
+                    customPayload);
 
-
-        public void LogOfferPurchaseEvent(string placementId, string offerId, string currencyCode, double totalAmount, Dictionary<string, object> customPayload = null)
-            => _ = _eventManager.QueueEventWithMeticaAttributesAsync(
-                UserId,
-                AppId,
-                placementId,
-                offerId,
-                EventTypes.OfferPurchase,
-                new() {
+            public static void LogOfferPurchaseEvent(string placementId, string offerId, string currencyCode, double totalAmount, Dictionary<string, object> customPayload = null)
+                => _ = Sdk._eventManager.QueueEventWithMeticaAttributesAsync(
+                    UserId,
+                    AppId,
+                    placementId,
+                    offerId,
+                    EventTypes.OfferPurchase,
+                    new() {
                     { nameof(currencyCode), currencyCode },
                     { nameof(totalAmount), totalAmount },
-                },
-                customPayload);
+                    },
+                    customPayload);
 
-
-        public void LogOfferPurchaseEventWithProductId(string productId, string currencyCode, double totalAmount, Dictionary<string, object> customPayload = null)
-            => _eventManager.QueueEventWithProductId(
-                UserId,
-                AppId,
-                productId,
-                EventTypes.OfferPurchase,
-                new() {
+            public static void LogOfferPurchaseEventWithProductId(string productId, string currencyCode, double totalAmount, Dictionary<string, object> customPayload = null)
+                => Sdk._eventManager.QueueEventWithProductId(
+                    UserId,
+                    AppId,
+                    productId,
+                    EventTypes.OfferPurchase,
+                    new() {
                     { nameof(currencyCode), currencyCode },
                     { nameof(totalAmount), totalAmount },
-                },
-                customPayload);
+                    },
+                    customPayload);
 
+            public static void LogOfferInteractionEvent(string placementId, string offerId, string interactionType, Dictionary<string, object> customPayload = null)
+                => _ = Sdk._eventManager.QueueEventWithMeticaAttributesAsync(
+                    UserId,
+                    AppId,
+                    placementId,
+                    offerId,
+                    EventTypes.OfferInteraction,
+                    new() { { nameof(interactionType), interactionType } },
+                    customPayload);
 
-        public void LogOfferInteractionEvent(string placementId, string offerId, string interactionType, Dictionary<string, object> customPayload = null)
-            => _ = _eventManager.QueueEventWithMeticaAttributesAsync(
-                UserId,
-                AppId,
-                placementId,
-                offerId,
-                EventTypes.OfferInteraction,
-                new() { { nameof(interactionType), interactionType } },
-                customPayload);
+            public static void LogOfferInteractionEventWithProductId(string productId, string interactionType, Dictionary<string, object> customPayload = null)
+                => Sdk._eventManager.QueueEventWithProductId(
+                    UserId,
+                    AppId,
+                    productId,
+                    EventTypes.OfferInteraction,
+                    new() { { nameof(interactionType), interactionType } },
+                    customPayload);
 
+            public static void LogOfferImpressionEvent(string placementId, string offerId, Dictionary<string, object> customPayload = null)
+                => _ = Sdk._eventManager.QueueEventWithMeticaAttributesAsync(
+                    UserId,
+                    AppId,
+                    placementId,
+                    offerId,
+                    EventTypes.OfferImpression,
+                    null,
+                    customPayload);
 
-        public void LogOfferInteractionEventWithProductId(string productId, string interactionType, Dictionary<string, object> customPayload = null)
-            => _eventManager.QueueEventWithProductId(
-                UserId,
-                AppId,
-                productId,
-                EventTypes.OfferInteraction,
-                new() { { nameof(interactionType), interactionType} },
-                customPayload);
+            public static void LogOfferImpressionEventWithProductId(string productId, Dictionary<string, object> customPayload = null)
+                => Sdk._eventManager.QueueEventWithProductId(
+                    UserId,
+                    AppId,
+                    productId,
+                    EventTypes.OfferImpression,
+                    null,
+                    customPayload);
 
-
-        public void LogOfferImpressionEvent(string placementId, string offerId, Dictionary<string, object> customPayload = null)
-            => _ = _eventManager.QueueEventWithMeticaAttributesAsync(
-                UserId,
-                AppId,
-                placementId,
-                offerId,
-                EventTypes.OfferImpression,
-                null,
-                customPayload);
-
-
-        public void LogOfferImpressionEventWithProductId(string productId, Dictionary<string, object> customPayload = null)
-            => _eventManager.QueueEventWithProductId(
-                UserId,
-                AppId,
-                productId,
-                EventTypes.OfferImpression,
-                null,
-                customPayload);
-
-
-        public void LogAdRevenueEvent(string placement, string type, string source, string currencyCode, double totalAmount, Dictionary<string, object> customPayload = null)
-            => _eventManager.QueueEventAsync(
-                UserId,
-                AppId,
-                EventTypes.AdRevenue,
-                new() {
+            public static void LogAdRevenueEvent(string placement, string type, string source, string currencyCode, double totalAmount, Dictionary<string, object> customPayload = null)
+                => Sdk._eventManager.QueueEventAsync(
+                    UserId,
+                    AppId,
+                    EventTypes.AdRevenue,
+                    new() {
                     { nameof (placement), placement },
                     { nameof (type), type },
                     { nameof (source), source },
                     { nameof (currencyCode), currencyCode },
                     { nameof (totalAmount), totalAmount },
-                },
-                customPayload);
+                    },
+                    customPayload);
 
+            public static void LogFullStateUserUpdateEvent(Dictionary<string, object> userStateAttributes, Dictionary<string, object> customPayload = null)
+                => Sdk._eventManager.QueueEventAsync(
+                    UserId,
+                    AppId,
+                    EventTypes.FullStateUpdate,
+                    new() { { nameof(userStateAttributes), userStateAttributes }, },
+                    customPayload);
 
-        public void LogFullStateUserUpdateEvent(Dictionary<string, object> userStateAttributes, Dictionary<string, object> customPayload = null)
-            => _eventManager.QueueEventAsync(
-                UserId,
-                AppId,
-                EventTypes.FullStateUpdate,
-                new() { { nameof (userStateAttributes), userStateAttributes }, },
-                customPayload);
+            public static void LogPartialStateUserUpdateEvent(Dictionary<string, object> userStateAttributes, Dictionary<string, object> customPayload = null)
+                => Sdk._eventManager.QueueEventAsync(
+                    UserId,
+                    AppId,
+                    EventTypes.PartialStateUpdate,
+                    new() { { nameof(userStateAttributes), userStateAttributes } },
+                    customPayload);
 
-
-        public void LogPartialStateUserUpdateEvent(Dictionary<string, object> userStateAttributes, Dictionary<string, object> customPayload = null)
-            => _eventManager.QueueEventAsync(
-                UserId,
-                AppId,
-                EventTypes.PartialStateUpdate,
-                new() { { nameof(userStateAttributes), userStateAttributes } },
-                customPayload);
-
-        public void LogCustomEvent(string customEventType, Dictionary<string, object> customPayload = null)
-        {
-            if (EventTypes.IsEventType(customEventType))
+            public static void LogCustomEvent(string customEventType, Dictionary<string, object> customPayload = null)
             {
-                Log.Error(() => $"{customEventType} cannot be used with {nameof(LogCustomEvent)}. Please use an event type that is not a core event. See documentation at https://docs.metica.com/integration#core-events.");
-                return;
+                if (EventTypes.IsEventType(customEventType))
+                {
+                    Log.Error(() => $"{customEventType} cannot be used with {nameof(LogCustomEvent)}. Please use an event type that is not a core event. See documentation at https://docs.metica.com/integration#core-events.");
+                    return;
+                }
+                Sdk._eventManager.QueueEventAsync(
+                UserId,
+                AppId,
+                customEventType,
+                null,
+                customPayload);
             }
-            _eventManager.QueueEventAsync(
-            UserId,
-            AppId,
-            customEventType,
-            null,
-            customPayload);
+            public static void RequestDispatchEvents()
+            {
+                _ = Sdk._eventManager.RequestDispatchEvents();
+            }
         }
 
-        public void RequestDispatchEvents()
-        {
-            _ = _eventManager.RequestDispatchEvents();
-        }
+        public static async ValueTask DisposeAsync()
+            => await Sdk.SdkDisposeAsync();
 
-        public async ValueTask DisposeAsync()
+        private async ValueTask SdkDisposeAsync()
         {
             if (_eventManager != null) await _eventManager.DisposeAsync();
             if (_offerManager != null) await _offerManager.DisposeAsync();
             if (_configManager != null) await _configManager.DisposeAsync();
             _http?.Dispose();
+            Sdk = null;
+        }
+
+        // ADS bridge
+        public static class Ads
+        {
+            public const string TAG = MeticaAds.TAG;
+
+            // public static Task<MeticaInitializationResult> InitializeAsync(MeticaConfiguration configuration)
+            //     => MeticaAds.InitializeAsync(configuration);
+
+            // public static Task<MeticaInitializationResult> InitializeWithResultAsync(MeticaConfiguration configuration)
+            //     => MeticaAds.InitializeWithResultAsync(configuration);
+
+            public static void SetLogEnabled(bool logEnabled)
+                => MeticaAds.SetLogEnabled(logEnabled);
+
+            public static void CreateBanner(string bannerAdUnitId, MeticaBannerPosition position)
+                => MeticaAds.CreateBanner(bannerAdUnitId, position);
+
+            public static void ShowBanner(string adUnitId)
+                => MeticaAds.ShowBanner(adUnitId);
+
+            public static void HideBanner(string adUnitId)
+                => MeticaAds.HideBanner(adUnitId);
+
+            public static void DestroyBanner(string adUnitId)
+                => MeticaAds.DestroyBanner(adUnitId);
+
+            public static void LoadInterstitial()
+                => MeticaAds.LoadInterstitial();
+
+            public static void ShowInterstitial()
+                => MeticaAds.ShowInterstitial();
+
+            public static bool IsInterstitialReady()
+                => MeticaAds.IsInterstitialReady();
+
+            public static void LoadRewarded()
+                => MeticaAds.LoadRewarded();
+
+            public static void ShowRewarded()
+                => MeticaAds.ShowRewarded();
+
+            public static bool IsRewardedReady()
+                => MeticaAds.IsRewardedReady();
+
+            public static void NotifyAdLoadAttempt(string interstitialAdUnitId)
+                => MeticaAds.NotifyAdLoadAttempt(interstitialAdUnitId);
+
+            public static void NotifyAdLoadSuccess(MeticaAd meticaAd)
+                => MeticaAds.NotifyAdLoadSuccess(meticaAd);
+
+            public static void NotifyAdLoadFailed(string adUnitId, string error)
+                => MeticaAds.NotifyAdLoadFailed(adUnitId, error);
+
+            public static void NotifyAdShowSuccess(MeticaAd meticaAd)
+                => MeticaAds.NotifyAdShowSuccess(meticaAd);
         }
     }
 }
