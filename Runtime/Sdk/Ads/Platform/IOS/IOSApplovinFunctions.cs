cfa7e77851d904969c49ef64ceafedf818630ac3
daniilmetica <daniil@metica.com>
2025-11-20 14:40:21 +0100

    added consent functions

--- Runtime/Sdk/Ads/Platform/IOS/IOSApplovinFunctions.cs
+++ Runtime/Sdk/Ads/Platform/IOS/IOSApplovinFunctions.cs
@@ -1,15 +1,33 @@
+#nullable enable
+
+using System;
+using System.Runtime.InteropServices;
+using Metica.Ads;
+
 namespace Metica.Ads.IOS
 {
     internal class IOSApplovinFunctions : MeticaApplovinFunctions
     {
+        private const string TAG = MeticaAds.TAG;
+        [DllImport("__Internal")]
+        private static extern bool ios_hasUserConsent();
+        [DllImport("__Internal")]
+        private static extern bool ios_isUserConsentSet();
+
         public bool HasUserConsent()
         {
-            return false;
+            MeticaAds.Log.LogDebug(() => $"{TAG} About to call iOS Max.hasUserConsent method");
+            var result = ios_hasUserConsent();
+            MeticaAds.Log.LogDebug(() => $"{TAG} iOS Max.hasUserConsent returned: {result}");
+            return result;
         }
 
         public bool IsUserConsentSet()
         {
-            return false;
+            MeticaAds.Log.LogDebug(() => $"{TAG} About to call iOS Max.isUserConsentSet method");
+            var result = ios_isUserConsentSet();
+            MeticaAds.Log.LogDebug(() => $"{TAG} iOS Max.isUserConsentSet returned: {result}");
+            return result;
         }
 
         public MaxSdk.ConsentFlowUserGeography GetConsentFlowUserGeography()
